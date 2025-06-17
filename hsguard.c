1f8353f8f2614a8c547ac91cf50bd9c4f2ec3bc5
Sir Hesti <hstools@t-online.de>
2025-06-17 11:03:06 +0200

    Di 17. Jun 11:02:51 CEST 2025

--- hsguard.c
+++ hsguard.c
@@ -461,14 +461,74 @@ Fehler (auch in dieser Manpage) unbedingt bitte melden
 
 Contens/Verzeichnis
 
-1. [Build from Source](BUILD)
+1. [Build from Source](#BUILD)
 1. Init
 1. daily
 
 <a name="BUILD">Build from Source</a>
 
+```
+@> git clone https://github.com/SirHesti/hsguard.git
+clone to 'hsguard'...
+remote: Enumerating objects: 193, done.
+[etc]
+@> cd hsguard
+@> cmake .
+```
+
+Maybe an Error occured __"SQLite3 not found"__ if sqlite3(dev) missing. Then enter
+```
+sudo apt install sqlite3 libsqlite3-dev
+```
+to solve this problem. run "cmake ." again!
 
 
+```
+@> make all
+-- The C compiler identification is GNU 12.2.0
+-- Detecting C compiler ABI info
+[etc]
+-- Build files have been written to: /@/hsguard
+@> sudo make install
+password: ***********
+[ 88%] Built target hsguard
+[100%] Built target man
+Install the project...
+-- Install configuration: ""
+-- Installing: /usr/bin/hsguard
+-- Installing: /usr/share/man/man8/hsguard.8.gz
+-- Installing: /usr/share/man/de/man8/hsguard.8.gz
+```
+we're done.
+
+<a name="CONFIG">Configure hsguard</a>
+
+To run hsguard, configuration is required. Simply let hsguard do this as follows:
+
+```
+@>hsguard
+[DATE] /etc/hsguard.rc not exists ....
+@>sudo hsguard --create
+password: ***********
+Sure you want to create /etc/hsguard.rc (y/n) ? Y
+Create Dir in /var/hsguard (y/n) ? Y
+generated /etc/hsguard.rc
+```
+
+Now see the config like:
+
+```
+DATABASE=/var/hsguard/hsguard.db
+BASE=/
+#ROOT/FAIL/USER/INFO/DEVL
+VERBOSE=USER
+#If desired, an empty file can be created with --createexcl
+#EXCLUDE=/var/hsguard/exclude.lst
+NOSCREEN=Y
+EMERGENCY=PANIC
+ANSICOLOR=Y
+COL_* can be delete. This is only need if you need 
+```
 
 
 
